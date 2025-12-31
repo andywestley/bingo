67f7d60c1cfa81f1e877d32a5e2a53be93c585f8
Andy Westley <andy@andrewwestley.co.uk>
2025-12-31 11:13:32 +0000

    Animation bug

--- player.php
+++ player.php
@@ -88,7 +88,7 @@
   </div>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 <script src="js/logger.js?v=<?php echo time(); ?>"></script>
