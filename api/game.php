0182c8cf3ecbc6f4c0abde8db3a7ac3f9db60392
Andy Westley <andy@andrewwestley.co.uk>
2025-12-31 09:59:07 +0000

    Player bug fixes

--- api/game.php
+++ api/game.php
@@ -2,9 +2,11 @@
 // api/game.php
 
 // Enable error reporting for debugging (disable in production)
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// Enable error reporting for debugging (disable in production)
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+ini_set('display_errors', 0);
 
 header('Content-Type: application/json');
 
--- player.php
+++ player.php
@@ -77,6 +77,8 @@
 </div>
 
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 <script src="js/logger.js?v=<?php echo time(); ?>"></script>
 <script src="js/bingo-client.js?v=<?php echo time(); ?>"></script>
 <script src="js/player.js?v=<?php echo time(); ?>"></script>
